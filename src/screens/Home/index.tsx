f5f97d0e9cf62135004b5f3bc44c230dc4613860
franciscomaik <chiconunes3081@gmail.com>
2024-10-24 08:36:23 -0300

    feat: Adicionar botão "Nova refeição" na tela inicial
    Adiciona o componente Button na tela inicial com o título "Nova refeição" e o ícone "plus".

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import * as S from "./styles";
+import { Button } from "@components/Button";
 
 const Home: React.FC = () => {
   const { top } = useSafeAreaInsets();
@@ -10,6 +11,10 @@ const Home: React.FC = () => {
   return (
     <S.Container style={{ marginTop: top }}>
       <Header />
+      <S.ContentButton>
+        <S.TitleSection>Refeições</S.TitleSection>
+        <Button title="Nova refeição" icon="plus" />
+      </S.ContentButton>
     </S.Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,4 +2,15 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   padding: 24px;
+  gap: 32px;
+`;
+
+export const ContentButton = styled.View`
+  gap: 8px;
+`;
+
+export const TitleSection = styled.Text`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  color: ${({ theme }) => theme.COLORS.GRAY_1};
 `;

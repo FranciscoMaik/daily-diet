9824255d97244f83faf597805cec681c5a01ed06
franciscomaik <chiconunes3081@gmail.com>
2024-10-25 18:59:54 -0300

    feat: Adicionar componente ButtonSelect para seleção de opções e atualizar componente Form
    Adiciona o componente ButtonSelect para permitir a seleção de opções e atualiza o componente Form para utilizar o novo componente. Também adiciona estados e funções para manipular a seleção de opções, além de adicionar campos de data e hora com validação de formato.
    
    Refatora o componente MealList para adicionar suporte ao toque em itens e espaçamento entre seções.

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
+import { useWindowDimensions } from "react-native";
+
 import type { IDiet } from "src/types/IDiet";
 
 import { InputWithLabel } from "@components/InputWithLabel";
 import { Button } from "@components/Button";
+import { ButtonSelect } from "@components/ButtonSelect";
 
 import * as S from "./styles";
-import { useWindowDimensions } from "react-native";
-
 interface FormProps {
   diet?: IDiet;
 }
@@ -13,15 +15,56 @@ interface FormProps {
 export const Form: React.FC<FormProps> = ({ diet }) => {
   const { width } = useWindowDimensions();
 
+  const [name, setName] = useState(diet?.name || "");
+  const [description, setDescription] = useState(diet?.description || "");
+  const [date, setDate] = useState(diet?.date || "");
+  const [hour, setHour] = useState(diet?.hour || "");
+  const [inDiet, setInDiet] = useState<"yes" | "not">("yes");
+
+  const handleSelect = (item: "yes" | "not") => {
+    if (item !== inDiet) {
+      setInDiet(item);
+    }
+  };
+
+  const handleChangeDate = (text: string) => {
+    const format = text
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d{2})(\d)/, "$1/$2/$3");
+    setDate(format);
+  };
+
+  const handleChangeHour = (text: string) => {
+    const format = text.replace(/\D/g, "").replace(/(\d{2})(\d)/, "$1:$2");
+    setHour(format);
+  };
+
   return (
     <S.Container>
       <S.BaseForm>
-        <InputWithLabel label="Nome" />
-        <InputWithLabel label="Descrição" numberOfLines={4} multiline={true} />
+        <InputWithLabel label="Nome" value={name} onChangeText={setName} />
+        <InputWithLabel
+          label="Descrição"
+          numberOfLines={4}
+          multiline={true}
+          value={description}
+          onChangeText={setDescription}
+        />
         <S.ContentDate>
-          <InputWithLabel label="Data" />
-          <InputWithLabel label="Hora" />
+          <InputWithLabel
+            label="Data"
+            value={date}
+            onChangeText={handleChangeDate}
+            maxLength={10}
+          />
+          <InputWithLabel
+            label="Hora"
+            value={hour}
+            onChangeText={handleChangeHour}
+            maxLength={5}
+          />
         </S.ContentDate>
+        <ButtonSelect itemSelected={inDiet} onSelect={handleSelect} />
       </S.BaseForm>
 
       <Button title="Cadastrar refeição" />

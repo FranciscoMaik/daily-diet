f892c6162175e69124f384bf59634fa444962b6a
franciscomaik <chiconunes3081@gmail.com>
2024-10-25 18:11:40 -0300

    feat: Adicionar rota para cadastro de dieta e navegação para nova dieta

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,14 @@
 import { Header } from "@components/Header";
-import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import * as S from "./styles";
 import { Button } from "@components/Button";
 import { MealList } from "@components/MealList";
+import { useNavigation } from "@react-navigation/native";
 
 const Home: React.FC = () => {
   const { top } = useSafeAreaInsets();
+  const { navigate } = useNavigation();
 
   return (
     <S.Container style={{ marginTop: top }}>
@@ -15,7 +16,11 @@ const Home: React.FC = () => {
 
       <S.ContentButton>
         <S.TitleSection>Refeições</S.TitleSection>
-        <Button title="Nova refeição" icon="plus" />
+        <Button
+          title="Nova refeição"
+          icon="plus"
+          onPress={() => navigate("newDiet")}
+        />
       </S.ContentButton>
 
       <MealList />

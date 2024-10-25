3fbda2bf27bc62fd33fb217f589cec502d41c5ad
franciscomaik <chiconunes3081@gmail.com>
2024-10-25 09:07:08 -0300

    feat: Adicionar componente MealList na tela inicial
    Adiciona o componente MealList na tela inicial para exibir as refeições cadastradas.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,7 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import * as S from "./styles";
 import { Button } from "@components/Button";
+import { MealList } from "@components/MealList";
 
 const Home: React.FC = () => {
   const { top } = useSafeAreaInsets();
@@ -11,10 +12,13 @@ const Home: React.FC = () => {
   return (
     <S.Container style={{ marginTop: top }}>
       <Header />
+
       <S.ContentButton>
         <S.TitleSection>Refeições</S.TitleSection>
         <Button title="Nova refeição" icon="plus" />
       </S.ContentButton>
+
+      <MealList />
     </S.Container>
   );
 };

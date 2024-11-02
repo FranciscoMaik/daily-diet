cea689fe43827b7523dc30192d9d850cfed279f0
franciscomaik <chiconunes3081@gmail.com>
2024-11-02 12:01:07 -0300

    feat: adding percentage in components

--- src/components/Percentage/index.tsx
+++ src/components/Percentage/index.tsx
@@ -1,25 +1,38 @@
+import { useEffect, useState } from "react";
 import Feather from "@expo/vector-icons/Feather";
 import { useNavigation } from "@react-navigation/native";
+import { useTheme } from "styled-components/native";
+import { useFocusEffect } from "@react-navigation/native";
+
+import { calculatePercentage } from "@utils/calculatePercentage";
 
 import * as S from "./styles";
-import { useTheme } from "styled-components/native";
-
-const basePercentage = 50;
 
 export const Percentage: React.FC = () => {
   const { navigate } = useNavigation();
   const { COLORS } = useTheme();
 
+  const [percentage, setPercentage] = useState(0);
+
+  useFocusEffect(() => {
+    async function getPercentage() {
+      const { percentage } = await calculatePercentage();
+      setPercentage(percentage);
+    }
+
+    getPercentage();
+  });
+
   return (
-    <S.Container basePercentage={basePercentage}>
+    <S.Container basePercentage={percentage}>
       <Feather
         name="arrow-up-right"
         size={28}
-        color={basePercentage >= 50 ? COLORS.GREEN_DARK : COLORS.RED_DARK}
+        color={percentage >= 50 ? COLORS.GREEN_DARK : COLORS.RED_DARK}
         style={{ position: "absolute", top: 8, right: 8 }}
         onPress={() => navigate("statistics")}
       />
-      <S.Title>90,86%</S.Title>
+      <S.Title>{percentage.toFixed(2)}%</S.Title>
       <S.Subtitle>das refeições dentro da dieta</S.Subtitle>
     </S.Container>
   );

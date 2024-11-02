a54ebbf93604e07ee1b1bbb3ced3d5427f4c80b6
franciscomaik <chiconunes3081@gmail.com>
2024-11-02 12:03:36 -0300

    feat: adding values statistics

--- src/screens/Statistics/index.tsx
+++ src/screens/Statistics/index.tsx
@@ -1,29 +1,49 @@
+import { useEffect, useState } from "react";
 import Feather from "@expo/vector-icons/Feather";
 import { useNavigation } from "@react-navigation/native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import * as S from "./styles";
 import { useTheme } from "styled-components/native";
 import { CardInfo } from "@components/CardInfo";
+import { calculatePercentage } from "@utils/calculatePercentage";
 
-const basePercentage = 40;
+import * as S from "./styles";
 
 const Statistics: React.FC = () => {
   const { goBack } = useNavigation();
   const { top } = useSafeAreaInsets();
   const { COLORS } = useTheme();
 
+  const [percentage, setPercentage] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [inDiet, setInDiet] = useState(0);
+  const [outDiet, setOutDiet] = useState(0);
+  const [bestSequence, setBestSequence] = useState(0);
+  useEffect(() => {
+    async function getPercentage() {
+      const { percentage, total, inDiet, outDiet, bestSequence } =
+        await calculatePercentage();
+      setPercentage(percentage);
+      setTotal(total);
+      setInDiet(inDiet);
+      setOutDiet(outDiet);
+      setBestSequence(bestSequence);
+    }
+
+    getPercentage();
+  }, []);
+
   return (
-    <S.Container basePercentage={basePercentage} mt={top}>
+    <S.Container basePercentage={percentage} mt={top}>
       <S.Header>
         <Feather
           name="arrow-left"
           size={28}
-          color={basePercentage >= 50 ? COLORS.GREEN_DARK : COLORS.RED_DARK}
+          color={percentage >= 50 ? COLORS.GREEN_DARK : COLORS.RED_DARK}
           style={{ position: "absolute", top: 24, left: 24 }}
           onPress={goBack}
         />
-        <S.Title>90,86%</S.Title>
+        <S.Title>{percentage.toFixed(2)}%</S.Title>
         <S.Subtitle>das refeições dentro da dieta</S.Subtitle>
       </S.Header>
 
@@ -31,23 +51,23 @@ const Statistics: React.FC = () => {
         <S.BodyTitle>Estatísticas gerais</S.BodyTitle>
         <S.ContentInfos>
           <CardInfo
-            value="22"
+            value={bestSequence.toString()}
             title="melhor sequência de pratos dentro da dieta"
             background={COLORS.GRAY_6}
           />
           <CardInfo
-            value="109"
+            value={total.toString()}
             title="refeições registradas"
             background={COLORS.GRAY_6}
           />
           <S.RowInfo>
             <CardInfo
-              value="99"
+              value={inDiet.toString()}
               title="refeições dentro da dieta"
               background={COLORS.GREEN_LIGHT}
             />
             <CardInfo
-              value="10"
+              value={outDiet.toString()}
               title="refeições fora da dieta"
               background={COLORS.RED_LIGHT}
             />

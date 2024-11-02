ca61afabd4688adb9eb48d136a60fba69ae913fd
franciscomaik <chiconunes3081@gmail.com>
2024-11-02 12:01:28 -0300

    feat: adding new screens in router

--- src/routes/public.routes.tsx
+++ src/routes/public.routes.tsx
@@ -4,8 +4,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "@screens/Home";
 import Statistics from "@screens/Statistics";
 import NewDiet from "@screens/NewDiet";
+import Details from "@screens/Details";
+import EditDiet from "@screens/EditDiet";
 
-const Stack = createNativeStackNavigator();
+import type { PublicStack } from "./types";
+
+const Stack = createNativeStackNavigator<PublicStack>();
 
 const Public: React.FC = () => {
   return (
@@ -18,6 +22,8 @@ const Public: React.FC = () => {
         <Stack.Screen name="home" component={Home} />
         <Stack.Screen name="statistics" component={Statistics} />
         <Stack.Screen name="newDiet" component={NewDiet} />
+        <Stack.Screen name="details" component={Details} />
+        <Stack.Screen name="editDiet" component={EditDiet} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,5 +1,13 @@
+import type { IDiet } from "src/types/IDiet";
+
 export type PublicStack = {
   home: undefined;
   statistics: undefined;
   newDiet: undefined;
+  details: {
+    id: string;
+  };
+  editDiet: {
+    diet: IDiet;
+  };
 };

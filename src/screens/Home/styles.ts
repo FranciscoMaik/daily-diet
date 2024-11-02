2b4c5dfec86fdd6b532322f4c411478bc6445f45
franciscomaik <chiconunes3081@gmail.com>
2024-11-02 13:09:23 -0300

    style: change spacing

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,10 +3,12 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   padding: 24px;
   gap: 32px;
+  flex: 1;
 `;
 
 export const ContentButton = styled.View`
   gap: 8px;
+  margin-bottom: 48px;
 `;
 
 export const TitleSection = styled.Text`

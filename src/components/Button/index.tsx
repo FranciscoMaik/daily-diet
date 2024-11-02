cf8320dff71ca5d6d8e9a25b00879acf16b8a606
franciscomaik <chiconunes3081@gmail.com>
2024-11-02 11:58:16 -0300

    style: create variant button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import type { TouchableOpacityProps } from "react-native";
+import { useTheme } from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
 
 import * as S from "./styles";
@@ -6,13 +7,29 @@ import * as S from "./styles";
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
   icon?: keyof typeof Feather.glyphMap;
+  variant?: "PRIMARY" | "SECONDARY";
 }
 
-export const Button: React.FC<ButtonProps> = ({ title, icon, ...rest }) => {
+export const Button: React.FC<ButtonProps> = ({
+  title,
+  icon,
+  variant = "PRIMARY",
+  ...rest
+}) => {
+  const theme = useTheme();
+
   return (
-    <S.Container {...rest} activeOpacity={0.7}>
-      {icon ? <Feather name={icon} size={18} color="#fff" /> : null}
-      <S.Title>{title}</S.Title>
+    <S.Container {...rest} activeOpacity={0.7} variant={variant}>
+      {icon ? (
+        <Feather
+          name={icon}
+          size={18}
+          color={
+            variant === "PRIMARY" ? theme.COLORS.WHITE : theme.COLORS.GRAY_1
+          }
+        />
+      ) : null}
+      <S.Title variant={variant}>{title}</S.Title>
     </S.Container>
   );
 };
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,29 @@
 import styled from "styled-components/native";
 
-export const Container = styled.TouchableOpacity`
+interface StyledButtonProps {
+  variant: "PRIMARY" | "SECONDARY";
+}
+
+export const Container = styled.TouchableOpacity<StyledButtonProps>`
   flex-direction: row;
   align-items: center;
   justify-content: center;
   gap: 12px;
   padding: 16px 24px;
+  flex: 1;
+  min-height: 52px;
+  max-height: 52px;
   border-radius: 6px;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_2};
+  background-color: ${({ theme, variant }) =>
+    variant === "PRIMARY" ? theme.COLORS.GRAY_2 : "transparent"};
+
+  border: 1px solid ${({ theme, variant }) =>
+    variant === "PRIMARY" ? "transparent" : theme.COLORS.GRAY_1};
 `;
 
-export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+export const Title = styled.Text<StyledButtonProps>`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
-  color: ${({ theme }) => theme.COLORS.WHITE};
+  color: ${({ theme, variant }) =>
+    variant === "PRIMARY" ? theme.COLORS.WHITE : theme.COLORS.GRAY_1};
 `;

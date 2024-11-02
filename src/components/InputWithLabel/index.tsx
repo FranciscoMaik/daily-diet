bca6c4cd0e178cec1a175bed31692fa34bde3e4d
franciscomaik <chiconunes3081@gmail.com>
2024-11-02 11:59:03 -0300

    style: adding color to placeholder

--- src/components/InputWithLabel/index.tsx
+++ src/components/InputWithLabel/index.tsx
@@ -1,4 +1,5 @@
 import type { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native";
 
 import * as S from "./styles";
 
@@ -11,10 +12,16 @@ export const InputWithLabel: React.FC<InputWithLabelProps> = ({
   multiline,
   ...rest
 }) => {
+  const theme = useTheme();
+
   return (
     <S.Container style={{ minHeight: multiline ? 120 : 70 }}>
       <S.Label>{label}</S.Label>
-      <S.Input multiline={multiline} {...rest} />
+      <S.Input
+        multiline={multiline}
+        {...rest}
+        placeholderTextColor={theme.COLORS.GRAY_4}
+      />
     </S.Container>
   );
 };
